d5c9e1f9ffaa397b9d829e1048ae4ec4f70f1419
共产主义接班人 <503407184@qq.com>
2021-08-20 19:23:55 +0800

    fix min & max

--- include/wow64pp.hpp
+++ include/wow64pp.hpp
@@ -23,6 +23,9 @@
 
 namespace wow64pp {
 
+#define NOMINMAX
+#include <Windows.h>
+    
     namespace defs {
 
         using NtQueryInformationProcessT =
@@ -851,7 +854,7 @@ namespace wow64pp {
 
         return ret;
     }
-
+#undef NOMINMAX
 } // namespace wow64pp
 
 #endif // #ifndef WOW64PP_HPP
